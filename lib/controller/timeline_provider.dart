cc81dcd295b1e512e49ee719a24a5bbbdf2c625e
Krishna Vishwakarma <krishnavishwakarma.og@gmail.com>
2022-12-24 13:32:40 +0530

    Small Fix

--- lib/controller/timeline_provider.dart
+++ lib/controller/timeline_provider.dart
@@ -2,33 +2,30 @@ import 'package:flutter/material.dart';
 import 'package:gettimeline/model/timeline_model.dart';
 
 class TimelineProvider extends ChangeNotifier {
-  Set<MyTimeLineModel> _timeline = {
+  Set<MyTimeLineModel> timeline = {
     MyTimeLineModel(label: 'Title 1'),
     MyTimeLineModel(label: 'Title 2'),
     MyTimeLineModel(label: 'Title 3'),
     MyTimeLineModel(label: 'Title 4'),
   };
 
-  Set<MyTimeLineModel> get timeline => _timeline;
-  set timeline(Set<MyTimeLineModel> value) {
-    _timeline = value;
+  void updateAllColor() {
+    timeline = timeline
+        .map((e) => MyTimeLineModel(
+              label: e.label,
+              color: e.title != null && e.title!.isNotEmpty
+                  ? Colors.green
+                  : e.title == null
+                      ? Colors.grey
+                      : Colors.red,
+              title: e.title,
+            ))
+        .toSet();
     notifyListeners();
   }
 
-  MyTimeLineModel _getTimeline(MyTimeLineModel e, Color c) =>
-      MyTimeLineModel(label: e.label, color: c, title: e.title);
-  void updateAllColor() {
-    timeline = _timeline
-        .map((e) => e.title != null && e.title!.isNotEmpty
-            ? _getTimeline(e, Colors.green)
-            : e.title == null
-                ? _getTimeline(e, Colors.grey)
-                : _getTimeline(e, Colors.red))
-        .toSet();
-  }
-
   void updateTitle(int index, String? title) {
-    _timeline.elementAt(index).title = title;
+    timeline.elementAt(index).title = title;
 
     notifyListeners();
   }
